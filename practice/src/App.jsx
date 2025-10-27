835db31629601c8c00949e1371d6f20f75a2c199
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-10-27 15:18:41 +0300

    props in  a react component

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -4,9 +4,9 @@ import Student  from "./Student"
 const App = () => {
   return (
     <div>
-      <Student name="Squidward" age="41" />
-      <Student name="Spongebob" />
-      <Student name="Patrick" />
+      <Student name="Squidward" age="41" isStudent={true} />
+      {/* <Student name="Spongebob" />
+      <Student name="Patrick" /> */}
     </div>
   )
 }
--- practice/src/Student.jsx
+++ practice/src/Student.jsx
@@ -4,7 +4,8 @@ const Student = (props) => {
   return (
     <div>
           <p>Name : { props.name}</p>
-          <p>Age : { props.age}</p>
+          <p>Age : {props.age}</p>
+          <p> Student : {props.isStudent? "yes" : "No"    }     </p>
     </div>
   )
 }
