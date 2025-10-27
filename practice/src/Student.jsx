3ca738957a54458fdb74c43c661a17a070bc0604
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-10-28 00:45:50 +0300

    props with react

--- practice/src/Student.jsx
+++ practice/src/Student.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 
-const Student = (props) => {
+const Student = ({name,age,image}) => {
   return (
-    <div>
-          <p>Name : { props.name}</p>
-          <p>Age : {props.age}</p>
-          <p> Student : {props.isStudent? "yes" : "No"    }     </p>
+    <div className="text-white rounded-2xl shadow-md texy-center p-4 overflow-hidden" >
+      <img className="w-full h-64 object-cover mb-4 rounded-lg" src={image} alt={name} />
+      <h2 className="text-2xl font-bold mb-2">{name}</h2>
+      <p className="text-lg">Age: {age}</p>
     </div>
   )
 }
