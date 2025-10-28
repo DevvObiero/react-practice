0d844330460783e9bb177fb722a9a70d0883f426
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-10-28 10:00:58 +0300

    I am learning about use State

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,21 +1,17 @@
-import Student  from "./Student"
 
+import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-4xl font-bold text-center my-8">Student Profiles</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 px-4">
-        <Student 
-          name="Alice Johnson" 
-          age={20} 
-          image="https://randomuser.me/api/portraits/women/44.jpg" 
-        />
-        <Student 
-          name="Bob Smith" 
-          age={22} 
-          image="https://randomuser.me/api/portraits/men/34.jpg" />
-      </div>
+      <h1 className="text-4xl font-bold text-center my-8">Use State Practice</h1>
+   
+      <button>
+-
+      </button>
+        <span>0</span>
+<button>+</button>
     </div>
   )
 }
