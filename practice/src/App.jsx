16d291e11c50e4f06072fea8204e756b1325fb2a
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-10-28 10:11:43 +0300

    increment and decrement implementation

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -3,15 +3,26 @@ import React from 'react'
 import { useState } from 'react'
 
 const App = () => {
+
+  const [count,setCount] =  useState(5)
+
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+
+  const decrement = () => {
+    setCount(count - 1 )
+  }
+
+
   return (
     <div>
       <h1 className="text-4xl font-bold text-center my-8">Use State Practice</h1>
    
-      <button>
--
-      </button>
-        <span>0</span>
-<button>+</button>
+      <button onClick={decrement}  >-</button>
+      <span>{count}</span>
+      <button onClick={increment}   >+</button>
     </div>
   )
 }
